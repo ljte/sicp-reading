b9acd04f19971e5783f7090754ed79517781b6c8
ljte <ljte823@gmail.com>
2025-12-10 22:35:30 +0300

    update main.rkt to the latest version

--- main.rkt
+++ main.rkt
@@ -5,4 +5,23 @@
 
 (square 5)
 
-(tesat)
+
+(define pi 3.14159)
+(define radius 10)
+
+(* pi (square radius))
+
+(define (circumference r)(* 2 pi r))
+
+(circumference radius)
+
+(define (addx x)
+  (define (add y) (+ x y))
+  add)
+
+(define addfive (addx 5))
+(define addten (addx 10))
+
+((addx 100)(addten (addfive 2)))
+
+ 
